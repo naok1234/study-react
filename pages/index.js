25997ece4ce8d73c8ab60df3b0a3d0b480e9ee9d
Naoki <pinkwhiteball@gmail.com>
2022-02-15 12:09:02 +0900

    ⚡️ useEffectを用いたマウント学習用

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import { Fotter } from '../components/Fotter'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      document.body.style.backgroundColor = "";
+    }
+  },[]);
+
   return (
     <div className={styles.container}>
       <Header/>
