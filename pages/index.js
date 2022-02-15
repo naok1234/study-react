1c05ad46dc23faeb8041d17c812412f7c61e9757
Naoki <pinkwhiteball@gmail.com>
2022-02-15 22:05:10 +0900

    ⚡️ state管理のためのuseStateお試し

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import { Fotter } from '../components/Fotter'
 import Header from '../components/Header'
 import Main from '../components/Main'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
+
+  const [foo,setFoo] = useState(1);
+
+  const hendleClick = (e) => {
+    setFoo((foo) => foo + 1);
+  };
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
@@ -16,6 +23,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header/>
+      <h1>{foo}</h1>
+      <button onClick={hendleClick}>click button!</button>
       <Main title="index"/>
       <Fotter />
     </div>
